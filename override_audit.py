b8948d6963a00519782070fe01ac49cd47256e47
Terence Martin <github@nurdz.com>
2017-01-20 21:55:16 -0800

    Use new method
    The current command for generating the result is now using the newly
    extracted method to generate its output.

--- override_audit.py
+++ override_audit.py
@@ -2,35 +2,12 @@ import sublime
 import sublime_plugin
 
 from .lib.packages import *
+from .lib.output_view import output_to_view
 
 ###-----------------------------------------------------------------------------
 
 class OverrideAuditListPluginsCommand(sublime_plugin.WindowCommand):
-    def get_view(self, title, syntax):
-        view = None
-        for tmpView in self.window.views ():
-            if tmpView.name () == title:
-                view = tmpView
-                break
-
-        if view is None:
-            view = self.window.new_file ()
-            view.set_scratch (True)
-            view.set_name (title)
-            view.assign_syntax (syntax)
-        else:
-            view.set_read_only (False)
-
-            view.sel ().clear ()
-            view.sel ().add (sublime.Region (0, view.size ()))
-            view.run_command ("left_delete")
-
-            if self.window.active_view () != view:
-                self.window.focus_view (view)
-
-        return view
-
-    def generate(self, view):
+    def run(self):
         pkg_list = PackageList ()
 
         caption = "Total Packages Installed: {}".format (len (pkg_list))
@@ -47,17 +24,11 @@ class OverrideAuditListPluginsCommand(sublime_plugin.WindowCommand):
                 "X" if pkg_info.unpacked_path is not None else " ",
                 pkg_name)
                 )
-        result.append (h_sep)
+        result.extend ([h_sep, ""])
 
-        view.run_command ("insert", {"characters": "\n".join (result)})
-        view.run_command ("move_to", {"to": "bof", "extend": False})
-        view.set_read_only (True)
-
-    def run(self):
-        title = "OverrideAudit: Package List"
-        syntax = "Packages/OverrideAudit/OverrideAudit-table.sublime-syntax"
-        view = self.get_view (title, syntax)
-
-        self.generate(view)
+        output_to_view (self.window,
+                        "OverrideAudit: Package List",
+                        result,
+                        syntax="Packages/OverrideAudit/OverrideAudit-table.sublime-syntax")
 
 ###-----------------------------------------------------------------------------
