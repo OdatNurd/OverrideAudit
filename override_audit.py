82f39e18546afce19451b545e61c3944dc6993fa
Terence Martin <github@nurdz.com>
2017-01-20 16:07:56 -0800

    Major Refactor
    Using the previous code as a template, now the WindowCommand can
    generate the table directly without invoking the tempoary TextCommand
    to do it.
    
    This still needs to delete the text in an existing tab, however.

--- override_audit.py
+++ override_audit.py
@@ -5,52 +5,6 @@ from .lib.packages import *
 
 ###-----------------------------------------------------------------------------
 
-class oa_impl_list_plugins(sublime_plugin.TextCommand):
-    def insert_title(self, pkg_list):
-        caption = "Total Packages Installed: %d" % len (pkg_list)
-        self.result.extend ([
-            caption,
-            "-" * len (caption),
-            "\n"
-            ])
-
-    def insert_header(self, pkg_list):
-        header = "| {:3} | {:3} | {:3} | {:<40} |".format ("Shp", "Ins", "Unp", "Package")
-        hLen = len (header)
-        self.result.extend ([
-            "-" * hLen,
-            header,
-            "-" * hLen,
-            ])
-        return hLen
-
-    def insert_record(self, pkg_info):
-        self.result.append (
-            "| [{:1}] | [{:1}] | [{:1}] | {:<40} |".format (
-            "X" if pkg_info.shipped_path is not None else " ",
-            "X" if pkg_info.installed_path is not None else " ",
-            "X" if pkg_info.unpacked_path is not None else " ",
-            pkg_info.name)
-            )
-
-    def insert_footer(self, pkg_list, length):
-        self.result.append ("-" * length)
-
-    def generate_list(self, pkg_list):
-        self.insert_title (pkg_list)
-        l = self.insert_header (pkg_list)
-        for name, info in pkg_list:
-            self.insert_record(info)
-        self.insert_footer (pkg_list, l)
-
-    def run(self, edit):
-        self.result = []
-        self.generate_list (PackageList ())
-        self.view.erase (edit, sublime.Region (0, self.view.size ()))
-        self.view.run_command ("insert", {"characters": "\n".join (self.result)})
-
-###-----------------------------------------------------------------------------
-
 class OverrideAuditListPluginsCommand(sublime_plugin.WindowCommand):
     def get_view(self, title, syntax):
         view = None
@@ -71,11 +25,32 @@ class OverrideAuditListPluginsCommand(sublime_plugin.WindowCommand):
 
         return view
 
+    def generate(self, view):
+        pkg_list = PackageList ()
+
+        caption = "Total Packages Installed: {}".format (len (pkg_list))
+        c_sep = "-" * len (caption)
+        header = "| {:3} | {:3} | {:3} | {:<40} |".format ("Shp", "Ins", "Unp", "Package")
+        h_sep = "-" * len (header)
+
+        result = [caption, c_sep, "", h_sep, header, h_sep]
+        for pkg_name, pkg_info in pkg_list:
+            result.append (
+                "| [{:1}] | [{:1}] | [{:1}] | {:<40} |".format (
+                "X" if pkg_info.shipped_path is not None else " ",
+                "X" if pkg_info.installed_path is not None else " ",
+                "X" if pkg_info.unpacked_path is not None else " ",
+                pkg_name)
+                )
+
+        view.run_command ("insert", {"characters": "\n".join (result)})
+        view.set_read_only (True)
+
     def run(self):
         title = "OverrideAudit: Package List"
         syntax = "Packages/OverrideAudit/OverrideAudit-table.sublime-syntax"
         view = self.get_view (title, syntax)
-        view.run_command ("oa_impl_list_plugins")
-        view.set_read_only (True)
+
+        self.generate(view)
 
 ###-----------------------------------------------------------------------------
