0bf08e156ac0a995a3ff9139ecee8426c403c091
Terence Martin <github@nurdz.com>
2017-01-26 15:41:33 -0800

    Clean up formatting
    Remove extra spaces between brackets and their call points. Although
    this style generally bugs me, in something as terse as python seems to
    be, it seems less egregious to do.

--- lib/packages.py
+++ lib/packages.py
@@ -25,7 +25,7 @@ class PackageInfo():
         self.unpacked_path = None
 
     def __repr__(self):
-        return "[name={0}, shipped={1}, installed={2}, unpacked={3}]".format (
+        return "[name={0}, shipped={1}, installed={2}, unpacked={3}]".format(
             self.name ,
             self.shipped_path,
             self.installed_path,
@@ -42,15 +42,15 @@ class PackageList():
     standard dictionary functionality such as iteration and content testing.
     """
     def __init__(self):
-        self.list = dict ()
+        self.list = dict()
 
-        exec_dir = os.path.dirname (sublime.executable_path ())
-        self.__get_package_list (os.path.join (exec_dir, "Packages"), shipped=True)
-        self.__get_package_list (sublime.installed_packages_path ())
-        self.__get_package_list (sublime.packages_path (), packed=False)
+        exec_dir = os.path.dirname(sublime.executable_path())
+        self.__get_package_list(os.path.join(exec_dir, "Packages"), shipped=True)
+        self.__get_package_list(sublime.installed_packages_path ())
+        self.__get_package_list(sublime.packages_path (), packed=False)
 
     def __len__(self):
-        return len (self.list)
+        return len(self.list)
 
     def __getitem__(self, key):
         return self.list[key]
@@ -63,7 +63,7 @@ class PackageList():
         if "Default" in self.list:
             yield "Default", self.list["Default"]
 
-        for pkg in sorted (self.list):
+        for pkg in sorted(self.list):
             if pkg in ["User", "Default"]:
                 continue
             yield pkg, self.list[pkg]
@@ -73,14 +73,14 @@ class PackageList():
 
     def __get_pkg(self, name):
         if name not in self.list:
-            self.list[name] = PackageInfo (name)
+            self.list[name] = PackageInfo(name)
 
         return self.list[name]
 
-    def __packed_package (self, path, name, shipped):
-        package_path = os.path.join (path, name)
-        package_mtime = os.path.getmtime (package_path)
-        pkg = self.__get_pkg (os.path.splitext(name)[0])
+    def __packed_package(self, path, name, shipped):
+        package_path = os.path.join(path, name)
+        package_mtime = os.path.getmtime(package_path)
+        pkg = self.__get_pkg(os.path.splitext(name)[0])
         if shipped:
             pkg.shipped_path = package_path
             pkg.shipped_mtime = package_mtime
@@ -88,19 +88,19 @@ class PackageList():
             pkg.installed_path = package_path
             pkg.installed_mtime = package_mtime
 
-    def __unpacked_package (self, path, name, shipped):
-        pkg = self.__get_pkg (name)
-        pkg.unpacked_path = os.path.join (path, name)
+    def __unpacked_package(self, path, name, shipped):
+        pkg = self.__get_pkg(name)
+        pkg.unpacked_path = os.path.join(path, name)
 
-    def __get_package_list (self, location, packed=True, shipped=False):
+    def __get_package_list(self, location, packed=True, shipped=False):
         # Follow symlinks since we're stopping after one level anyway
-        for (path, dirs, files) in os.walk (location, followlinks=True):
+        for (path, dirs, files) in os.walk(location, followlinks=True):
             if packed:
-                for name in [f for f in files if f.endswith (".sublime-package")]:
-                    self.__packed_package (path, name, shipped)
+                for name in [f for f in files if f.endswith(".sublime-package")]:
+                    self.__packed_package(path, name, shipped)
             else:
                 for name in dirs:
-                    self.__unpacked_package (path, name, shipped)
+                    self.__unpacked_package(path, name, shipped)
             break
 
 ###-----------------------------------------------------------------------------
--- override_audit.py
+++ override_audit.py
@@ -8,30 +8,30 @@ from .lib.output_view import output_to_view
 
 class OverrideAuditListPackagesCommand(sublime_plugin.WindowCommand):
     def run(self):
-        settings = sublime.load_settings ("OverrideAudit.sublime-settings")
-        pkg_list = PackageList ()
+        settings = sublime.load_settings("OverrideAudit.sublime-settings")
+        pkg_list = PackageList()
 
-        caption = "Total Packages Installed: {}".format (len (pkg_list))
-        c_sep = "=" * len (caption)
-        header = "| {:3} | {:3} | {:3} | {:<40} |".format ("Shp", "Ins", "Unp", "Package")
-        h_sep = "-" * len (header)
+        caption = "Total Packages Installed: {}".format(len(pkg_list))
+        c_sep = "=" * len(caption)
+        header = "| {:3} | {:3} | {:3} | {:<40} |".format("Shp", "Ins", "Unp", "Package")
+        h_sep = "-" * len(header)
 
         result = [caption, c_sep, "", h_sep, header, h_sep]
         for pkg_name, pkg_info in pkg_list:
             result.append (
-                "| [{:1}] | [{:1}] | [{:1}] | {:<40} |".format (
+                "| [{:1}] | [{:1}] | [{:1}] | {:<40} |".format(
                 "X" if pkg_info.shipped_path is not None else " ",
                 "X" if pkg_info.installed_path is not None else " ",
                 "X" if pkg_info.unpacked_path is not None else " ",
                 pkg_name)
                 )
-        result.extend ([h_sep, ""])
+        result.extend([h_sep, ""])
 
-        output_to_view (self.window,
-                        "OverrideAudit: Package List",
-                        result,
-                        reuse=settings.get ("reuse_views", True),
-                        clear=settings.get ("clear_existing", True),
-                        syntax="Packages/OverrideAudit/syntax/OverrideAudit-table.sublime-syntax")
+        output_to_view(self.window,
+                       "OverrideAudit: Package List",
+                       result,
+                       reuse=settings.get("reuse_views", True),
+                       clear=settings.get("clear_existing", True),
+                       syntax="Packages/OverrideAudit/syntax/OverrideAudit-table.sublime-syntax")
 
 ###-----------------------------------------------------------------------------
