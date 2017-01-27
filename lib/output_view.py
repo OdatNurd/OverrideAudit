d0a56a95303d181b91ccef5f7b11189fc0105038
Terence Martin <github@nurdz.com>
2017-01-26 16:05:00 -0800

    Refactor and reformat the output view code
    This breaks up the main method into a few little pieces, which makes
    the result look a lot nicer to the eye (methods that can't fit nicely
    in a couple dozen likes bug me like all get out).
    
    This may also be slighly reusable for others.

--- lib/output_view.py
+++ lib/output_view.py
@@ -3,6 +3,75 @@ import sublime_plugin
 
 ###-----------------------------------------------------------------------------
 
+def find_view(window, title):
+    """
+    Attempt to find a view with the given title (name) in the given window
+    """
+    for view in window.views():
+        if view.name() == title:
+            return view
+
+def new_scratch_view(window, title, syntax=None):
+    """
+    Create a new view in the given window, giving it a name and an optional
+    syntax.
+    """
+    view = window.new_file()
+    view.set_scratch(True)
+    view.set_name(title)
+
+    if syntax is not None:
+        view.assign_syntax(syntax)
+
+    return view
+
+def clear_view(view):
+    """
+    Clear view contents entirely. Also returns it to single selection.
+    """
+    view.sel().clear()
+    view.sel().add(sublime.Region(0, view.size ()))
+    view.run_command("left_delete")
+
+def append_text(view, text):
+    """
+    Append the string provided to the end of the given view. This modifies the
+    selection to be single selection and leaves the cursor at the end of the
+    newly appended text.
+    """
+    view.sel().clear()
+    view.sel().add(sublime.Region (view.size(), view.size()))
+    view.run_command("insert", {"characters": text})
+
+def _save_state(view):
+    """
+    Save ths current size, selection and viewport position for the provided
+    view.
+    """
+    return (view.size(), list(view.sel()), view.viewport_position())
+
+def _restore_state(view,state):
+    """
+    Restore the selection and viewport position that was previously saved via
+    _save_state(). If the last selection was at the end of the buffer, put it
+    back there even if the size of the buffer has changed so that future append
+    operations will work.
+    """
+    size = state[0]
+    sel = state[1]
+    vpos = state[2]
+
+    # If the last selection was at the end of the buffer, replace that selection
+    # with the new end of the buffer so the relative position remains the same.
+    if sublime.Region(size, size) == sel[-1]:
+        sel[-1] = sublime.Region(view.size(), view.size())
+
+    view.sel().clear()
+    for region in sel:
+        view.sel().add(region)
+
+    view.set_viewport_position(vpos, False)
+
 def output_to_view(window,
                    title,
                    content,
@@ -10,61 +79,50 @@ def output_to_view(window,
                    syntax=None,
                    clear=True,
                    settings=None):
+    """
+    Add the content provided to a view in the given window, which has the title
+    provided. This will create a new view unless one with the title provided
+    already exists and resuse is true.
+
+    If an existing view is used, clear indicates if the current content should
+    be cleared or not before adding the new data.
+
+    When a new view is created, the optional syntax and settings will be applied
+    to the newly created view; when a view is reused, it is assumed that these
+    have already been set up.
+
+    The text will be appended to the end of the buffer. Care is taken to ensure
+    that the cursor position, view position and selection are maintained when
+    this is invoked.
+
+    This call leaves the output view in a read-only state; it is not neccesary
+    to turn this off if you invoke this method a second time and reuse a view.
+    """
 
     if not isinstance(content, str):
-        content = "\n".join (content)
+        content = "\n".join(content)
 
-    view = None
-
-    if reuse:
-        for _view in window.views ():
-            if _view.name () == title:
-                view = _view
-                break
+    view = find_view(window, title) if (reuse) else None
 
     if view is None:
-        view = window.new_file ()
-        view.set_scratch (True)
-        view.set_name (title)
-        if syntax is not None:
-            view.assign_syntax (syntax)
-
+        view = new_scratch_view(window, title, syntax)
     else:
-        view.set_read_only (False)
+        view.set_read_only(False)
 
         if clear is True:
-            view.sel ().clear ()
-            view.sel ().add (sublime.Region (0, view.size ()))
-            view.run_command ("left_delete")
+            clear_view(view)
 
-        if window.active_view () != view:
-            window.focus_view (view)
+        if window.active_view() != view:
+            window.focus_view(view)
 
     if settings is not None:
         for setting in settings:
-            view.settings ().set (setting, settings[setting])
+            view.settings().set(setting, settings[setting])
 
-    # Sace current buffer size, selection information and view position
-    saved_size = view.size ()
-    saved_sel = list(view.sel ())
-    saved_position = view.viewport_position ()
+    state = _save_state(view)
+    append_text(view, content)
+    _restore_state(view, state)
 
-    # Single select, position cursor at end of file, insert the data
-    view.sel ().clear ()
-    view.sel ().add (sublime.Region (saved_size, saved_size))
-    view.run_command ("insert", {"characters": content})
-
-    # If the last selection was at the end of the buffer, replace that selection
-    # with the new end of the buffer so the relative position remains the same.
-    if sublime.Region (saved_size, saved_size) == saved_sel[-1]:
-        saved_sel[-1] = sublime.Region (view.size (), view.size ())
-
-    # Clear current selection and add original selection back
-    view.sel ().clear ()
-    for region in saved_sel:
-        view.sel ().add (region)
-
-    view.set_viewport_position (saved_position, False)
-    view.set_read_only (True)
+    view.set_read_only(True)
 
 ###-----------------------------------------------------------------------------
