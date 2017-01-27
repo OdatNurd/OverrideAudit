b202dafacbd2e61105b64e920d07731a8fd13047
Terence Martin <github@nurdz.com>
2017-01-27 11:22:29 -0800

    Include dependency information in package list
    This adds a count of the number of packages that are actually
    dependencies and not standalone packages. Additionally this is
    signaled in the package list by wrapping the package name in angle
    brackets.

--- override_audit.py
+++ override_audit.py
@@ -13,7 +13,7 @@ class OverrideAuditListPackagesCommand(sublime_plugin.WindowCommand):
 
         settings = sublime.load_settings("OverrideAudit.sublime-settings")
 
-        title = "Packages: {}".format(len(pkg_list))
+        title = "Packages: {} ({} dependencies)".format(len(pkg_list), pkg_counts[4])
         t_sep = "=" * len(title)
 
         stats = ("Shipped:   {:<6} (Shipped with Sublime)\n"
@@ -28,6 +28,8 @@ class OverrideAuditListPackagesCommand(sublime_plugin.WindowCommand):
         for pkg_name, pkg_info in pkg_list:
             if pkg_info.disabled:
                 pkg_name = "[{}]".format (pkg_name)
+            elif pkg_info.is_dependency:
+                pkg_name = "<{}>".format (pkg_name)
             result.append (
                 "| [{:1}] | [{:1}] | [{:1}] | {:<40} |".format(
                 "S" if pkg_info.shipped_path is not None else " ",
