2140f5258710fcad35bbac0d3bce28120e4d85e1
Terence Martin <github@nurdz.com>
2017-02-03 22:19:06 -0800

    Make override_audit_diff_override actually useful
    This command still takes a "package" and "file" argument, but both are
    now optional.
    
    When no package is provided, the command will show a list of all
    packages that have at least one override, and after selecting a
    package a list will be presented for all of the overrides that exist
    in that particular package.
    
    When a package is provided, the command directly shows the list of
    overrides within that particular package for you to select from.
    
    In either case, selecting an override from the list invokes the diff
    for that particular override.
    
    These options allow for key commands, menu entries or command palette
    items custom tailored to any particular use case.
    
    This needs a bit of work still. In particular, when there are no
    packages that have overrides or no overrides inside a package (which
    can only happen if you specify a package directly), the situation is
    sent to the console but that's it.
    
    I'm not sure if the best thing to do is an error dialog, pop up the
    panel anyway with a single "No stuff" entry, the status line showing a
    message, or some combination thereof.

--- override_audit.py
+++ override_audit.py
@@ -7,23 +7,56 @@ from .lib.output_view import output_to_view
 ###-----------------------------------------------------------------------------
 
 class OverrideAuditDiffOverrideCommand(sublime_plugin.WindowCommand):
-    def run(self, package, file):
-        pkg_list = PackageList()
+    def _perform_diff(self, pkg_info, file):
         settings = sublime.load_settings("OverrideAudit.sublime-settings")
-
-        if package not in pkg_list:
-            print("No '%s' package found; cannot diff '%s'" % (package, file))
-            return
-
-        pkg_info = pkg_list[package]
         diff_info = pkg_info.override_diff(file, settings.get("diff_context_lines", 3))
+
         if diff_info is not None:
             output_to_view(self.window,
-               "Override of %s" % os.path.join(package, file),
+               "Override of %s" % os.path.join(pkg_info.name, file),
                diff_info,
                reuse=settings.get("reuse_views", True),
                clear=settings.get("clear_existing", True),
                syntax="Packages/Diff/Diff.sublime-syntax")
+
+    def _file_pick(self, pkg_info, override_list, index):
+        if index >= 0:
+            self._perform_diff(pkg_info, override_list[index])
+
+    def _show_override_list(self, pkg_info):
+        override_list = list(pkg_info.override_files())
+        if not override_list:
+            print("Package '%s' has no overrides" % pkg_info.name)
+        self.window.show_quick_panel(
+            items=override_list,
+            on_select=lambda i: self._file_pick(pkg_info, override_list, i))
+
+    def _pkg_pick(self, pkg_list, pkg_override_list, index):
+        if index >= 0:
+            self._show_override_list(pkg_list[pkg_override_list[index]])
+
+    def _show_pkg_list(self, pkg_list):
+        items = [name for name,pkg in pkg_list if len(pkg.override_files()) > 0]
+        if not items:
+            print("No packages have overrides")
+        self.window.show_quick_panel(
+                items=items,
+                on_select=lambda i: self._pkg_pick(pkg_list, items, i))
+
+    def run(self, package=None, file=None):
+        pkg_list = PackageList()
+
+        if package is None:
+            self._show_pkg_list(pkg_list)
+        else:
+            if package in pkg_list:
+                pkg_info = pkg_list[package]
+                if file is None:
+                    self._show_override_list(pkg_info)
+                else:
+                    self._perform_diff(pkg_info, file)
+            else:
+                print("Unable to diff; no such package '%s'" % package)
 
 ###-----------------------------------------------------------------------------
 
